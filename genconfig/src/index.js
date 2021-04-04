3afc83687666b5b3fdbb1c02f8027c73d2f5727a
Harald Fielker <harald.fielker@gmail.com>
2021-04-04 23:27:45 +0200

    cli parser

--- genconfig/src/index.js
+++ genconfig/src/index.js
@@ -1,8 +1,55 @@
 "use strict";
 
-const { strict } = require('assert');
 const axios = require('axios');
 const fs = require('fs');
+
+const { program } = require('commander');
+const packageData = require('./package.json');
+
+program.version(packageData.version);
+
+
+program
+  .option('-d, --debug', 'output extra debugging')
+  .option('-s, --small', 'small pizza size')
+  .option('-p, --pizza-type <type>', 'flavour of pizza')
+  .requiredOption('-c, --configFile', 'config file');
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+if (options.debug) {
+    console.log(options);
+}
+
+console.log('pizza details:');
+
+if (options.small) {
+    console.log('- small pizza size');
+}
+
+if (options.pizzaType) {
+    console.log(`- ${options.pizzaType}`);
+}
+
+/*
+$ pizza-options -d
+{ debug: true, small: undefined, pizzaType: undefined }
+pizza details:
+$ pizza-options -p
+error: option '-p, --pizza-type <type>' argument missing
+$ pizza-options -ds -p vegetarian
+{ debug: true, small: true, pizzaType: 'vegetarian' }
+pizza details:
+- small pizza size
+- vegetarian
+$ pizza-options --pizza-type=cheese
+pizza details:
+- cheese
+*/
+
+return;
 
 const openApiPaths = {};
 const openApiComponents = {};
