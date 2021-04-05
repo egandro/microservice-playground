f9e73cf2dea8198c8b3436491463111e4eb9d017
Harald Fielker <harald.fielker@gmail.com>
2021-04-05 14:53:40 +0200

    added tsoa example

---
+++ microservices/tsoaapi/src/src/app.ts
@@ -0,0 +1,26 @@
+import express, { Response as ExResponse, Request as ExRequest } from "express";
+import swaggerUi, { SwaggerUiOptions } from "swagger-ui-express";
+
+import bodyParser from "body-parser";
+import { RegisterRoutes } from "../build/routes";
+
+export const app = express();
+
+// Use body parser to read sent json payloads
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+RegisterRoutes(app);
+
+// no idea how do this better...
+app.use('/docs/swagger.json', express.static(__dirname + '/../swagger.json'));
+
+app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+  return res.send(
+    swaggerUi.generateHTML(await import("../build/swagger.json"))
+  );
+});
